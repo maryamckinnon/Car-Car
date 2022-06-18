a85dc21bbbe2115f37639d87799ebfae810def41
Jacob <jgcodingstuff@gmail.com>
2022-06-17 19:33:17 -0500

    Finished the models, started the views, got tired.

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from sales.api.common.json import ModelEncoder
+from .models import AutomobileVO, SalesPerson, Customer, SalesRecord
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+
+
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = []
+
+
+def api_sales_person(request):
+    
