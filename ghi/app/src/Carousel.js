14521222fc4f56521f185ec3a198b1d5ce658df5
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-08-19 01:05:37 -0700

    Added carousel with images

--- ghi/app/src/Carousel.js
+++ ghi/app/src/Carousel.js
@@ -1,6 +1,7 @@
 import { Carousel } from "react-bootstrap";
 import red from "./images/red.jpg";
-// import white from "./images/white.jpg";
+import blue from "./images/blue.jpg";
+import white from "./images/white.jpg";
 
 function CarouselFade() {
   return (
@@ -8,7 +9,23 @@ function CarouselFade() {
       <Carousel.Item style={{ zIndex: -1 }}>
         <Carousel.Caption>
           <div className="text-center">
-            <h1 className="display-5 fw-bold text-on-image" style={{fontSize: "25px", fontStyle: "italic"}}>
+            <h1 className="display-5 fw-bold text-on-image" style={{fontSize: "25px"}}>
+              The premiere solution for automobile dealership management
+            </h1>
+          </div>
+        </Carousel.Caption>
+        <img
+          className="d-block"
+          style={{ overflow: "hidden" }}
+          src={blue}
+          alt="First slide"
+        />
+      </Carousel.Item>
+
+      <Carousel.Item style={{ zIndex: -1 }}>
+        <Carousel.Caption>
+          <div className="text-center">
+            <h1 className="display-5 fw-bold text-on-image" style={{fontSize: "25px"}}>
               The premiere solution for automobile dealership management
             </h1>
           </div>
@@ -17,47 +34,25 @@ function CarouselFade() {
           className="d-block"
           style={{ overflow: "hidden" }}
           src={red}
-          alt="First slide"
+          alt="Second slide"
         />
       </Carousel.Item>
 
-      {/* <Carousel.Item style={{ zIndex: -1 }}>
+      <Carousel.Item style={{ zIndex: -1 }}>
         <Carousel.Caption>
           <div className="text-center">
-            <h1 className="display-5 fw-bold text-on-image">
-              CarCar
+            <h1 className="display-5 fw-bold text-on-image" style={{fontSize: "25px"}}>
+            The premiere solution for automobile dealership management
             </h1>
-            <p className="carousel-p">
-              The premiere solution for automobile dealership management
-            </p>
           </div>
         </Carousel.Caption>
         <img
           className="d-block"
           style={{ overflow: "hidden" }}
           src={white}
-          alt="Second slide"
-        />
-      </Carousel.Item> */}
-
-      {/* <Carousel.Item style={{ zIndex: -1 }}>
-        <Carousel.Caption>
-          <div className="text-center">
-            <h1 className="display-5 fw-bold text-on-image">
-              CarCar
-            </h1>
-            <p>
-              The premiere solution for automobile dealership management!
-            </p>
-          </div>
-        </Carousel.Caption>
-        <img
-          className="d-block"
-          style={{ overflow: "hidden" }}
-          src={nycSkyline}
           alt="Third slide"
         />
-      </Carousel.Item> */}
+      </Carousel.Item>
     </Carousel>
   );
 }
