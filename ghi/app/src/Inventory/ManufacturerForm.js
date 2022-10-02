166d72a0efda28dbac3438fee6da5d970dcac885
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-10-02 01:30:21 -0400

    Removed event prevent default so page can refresh after adding new manufacturer

--- ghi/app/src/Inventory/ManufacturerForm.js
+++ ghi/app/src/Inventory/ManufacturerForm.js
@@ -5,12 +5,10 @@ class ManufacturerForm extends React.Component {
       super(props)
       this.state = {
         name: '',
-        showForm: 'shadow p-4 mt-4',
-        showSuccess: 'd-none',
       };
       this.handleNameChange = this.handleNameChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
-      this.handleReset = this.handleReset.bind(this);
+      this.handleDelete = this.handleDelete.bind(this);
     }
 
     handleNameChange(event) {
@@ -18,18 +16,12 @@ class ManufacturerForm extends React.Component {
       this.setState({name: value})
     }
 
-    handleReset(event) {
-      this.setState(
-          {showForm: 'shadow p-4 mt-4',
-          showSuccess: 'd-none'}
-      );
+    handleDelete(event) {
+      const data = {...this.state};
     }
 
     async handleSubmit(event) {
-      event.preventDefault();
       const data = {...this.state};
-      delete data.showForm;
-      delete data.showSuccess;
 
       const manufacturerUrl = 'http://localhost:8100/api/manufacturers/';
         const fetchConfig = {
@@ -43,12 +35,15 @@ class ManufacturerForm extends React.Component {
         if (response.ok) {
           const cleared = {
             name: '',
-            showForm: 'shadow p-4 mt-4 d-none',
             showSuccess: '',
           };
           this.setState(cleared);
         } else {
-          console.error('invalid request')
+          window.alert('Invalid request. Please try again.')
+          const cleared = {
+            name: '',
+          };
+          this.setState(cleared);
       }
     }
     
@@ -56,23 +51,15 @@ class ManufacturerForm extends React.Component {
       return (
           <div className='row'>
           <div className='offset-3 col-6'>
-          <div className={this.state.showSuccess}>
-                        <div className="alert alert-success mt-4" role="alert">
-                            Manufacturer has been added
-                        </div>
-                        <button className="btn btn-outline-success" onClick={this.handleReset}>
-                            Add another manufacturer
-                        </button>
-                    </div>
-            {/* <div className={this.state.showForm}> */}
+              <div className='manufacturer-form'>
               <form onSubmit={this.handleSubmit} id='create-manufacturer-form'>
                   <input onChange={this.handleNameChange} placeholder='Add manufacturer' required type='text'
-                  name='name' id='name' value={this.state.name}/>
-                  <label htmlFor='name'><button className='btn btn-primary'>Add</button></label>
+                  name='name' id='name' value={this.state.name}/><button className='btn btn-primary'>Add</button>
+                  <label htmlFor='name'></label>
               </form>
+              </div>
             </div>
           </div>
-        // </div>
       );
   }
 }
